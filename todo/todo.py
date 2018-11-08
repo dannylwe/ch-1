cad5957abe38eee35b9a23662a43b5417e352ffc
Daniel Lwetabe <dannylwe@gmail.com>
2018-11-08 11:55:43 +0300

    Inital data structures

---
+++ todo/todo.py
@@ -0,0 +1,15 @@
+m_hometodo=['wakeup', 'wash the dog']
+m_schooltodo=['learn html', 'learn python']
+
+j_hometodo = [] 
+j_schooltodo = []
+ja_hometodo = []
+ja_schooltodo = []
+
+todos = {
+
+	"mary": [m_hometodo, m_schooltodo],
+	"john": [j_hometodo, j_schooltodo],
+	"jane": [ja_hometodo, ja_schooltodo],
+	
+}
---
+++ todo_func/user_todo.py
@@ -0,0 +1,42 @@
+users = []
+
+def create_user(username):
+	users.append(username)
+	return
+
+def create_todolist(username, todolist_name):
+	if username in users:
+		list(todolist_name)
+		global todo_name
+		todo_name = todolist_name.copy()
+
+		global todo 
+
+		todo = {
+		"username": username,
+		"todolist_name": todolist_name,
+		}
+
+		return todo_name
+
+def create_task(user_name, todolist_name, task_description):
+	create_user(username)
+	create_todolist(username, todolist_name)
+	todo_name.append(task_description)
+	return
+
+def remove_user(user_name):
+	users.remove(user_name)
+	return
+
+def remove_task_from_list(todolist_name, task):
+	todolist_name.remove(task)
+	return
+
+def remove_all_tasks(todolist_name):
+	for x in todolist_name:
+		todolist_name.remove(x)
+	return
+
+def get_user_tasks(user_name, no_of_tasks):
+	pass
