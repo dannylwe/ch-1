e1e09c2ca0a6f08b124d067591e3af016115b462
Daniel Lwetabe <dannylwe@gmail.com>
2018-11-10 11:50:04 +0300

    inital flask

--- api/app.py
+++ api/app.py
@@ -1,27 +1,12 @@
-from flask import Flask, abort, request, jsonify
-from flask_restplus import Api, Resource, fields, abort
+from flask import Flask, abort, request, jsonify, abort
 from werkzeug.contrib.fixers import ProxyFix
 #import uuid
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app)
-api = Api(app, version='1.0', title='sendIT api',
-    description='sendIT MVC api',
-)
-
 base_url= '/api/v1'
 
-app.config['RESTPLUS_VALIDATE'] = True
 app.config['DEBUG'] = True
-
-a_parcel = api.model('parcel', {	
-	"nickname": fields.String('Nickname of parcel'), 
-	"height": fields.Integer('Height of parcel'),
-	"width": fields.Integer('Width of parcel'),
-	"destination": fields.String('Destination of parcel'),
-	"pickup": fields.String('Pickup location of parcel'),
-
-})
 
 parcels = []
 
@@ -43,117 +28,111 @@ class Parcel:
 	@classmethod
 	def create(self, payload):
 		post = payload
-		post['id'] = parcels[-1]['id'] + 1 if len(parcels) > 0 else 0
+		post['id'] = parcels[-1]['id'] + 1 if len(parcels) > 1 else 1
 		post['status'] = Parcel.parcel_status
+		post['user_id'] = len(parcels) + 1
 		#post['uuid'] = Parcel.uq
 		parcels.append(post)
 		return parcels
 
-	@classmethod
-	def updates(self, id, payload):
-		# post = payload
-		# if post['id'] != parcels['id']:
-		# 	return {"item": "not found"}
-		# post.update(payload)
-		# return parcels
-		# data = payload
-		# for new_post in parcels:
-		# 	if data['id'] not in new_post['id']:
-		# 		raise ValueError('Invalid search') 
-		# 	new_post.update(data)
-		# 	return parcels
-		result = [prod for prod in parcels if prod['id'] == id]
-		final = result.update()
+	# @classmethod
+	# def updates(self, id, payload):
+	
+	# 	result = [prod for prod in parcels if prod['id'] == id]
+	# 	final = result.update()
 
 
-	@classmethod
-	def get_by_id(self, id):
-		result = [prod for prod in parcels if prod['id'] == id]
-		return result
+	# @classmethod
+	# def get_by_id(self, id):
+	# 	result = [prod for prod in parcels if prod['id'] == id]
+	# 	return result
 
-@api.route(base_url + '/hello')
-class HelloWorld(Resource):
+@app.route(base_url + '/hello')
+def hello_world():
+	return {'hello': 'world'}
 
-	def get(self):
-
-		"sanity check"
-
-		return {'hello': 'world'}
-
-@api.route(base_url + '/parcel')
-class Parcel_info(Resource):
-
-	def get(self):
-
-		"get all parcel info"
-
+@app.route(base_url + '/parcel', methods=['GET', 'POST'])
+def get_parcel():
+	if request.method == 'GET':
 		if parcels == []:
-			return {"message": "list is empty"}, 200
+			return jsonify({"message": "list is empty"}), 200
 
-		return parcels, 200
+		return jsonify({parcels}), 200
 
-	@api.expect(a_parcel, Validate=True)
-	def post(self):
+	if request.method == 'POST':
+		post_parcel = request.get_json()
+		Parcel.create(post_parcel)
 
-		"post parcel information"
-		
-		post = api.payload
-		Parcel.create(post)
+		return jsonify({"created": post_parcel}), 201
 
-		return {"created": post}, 201
+@app.route(base_url + '/parcel/<int:id>', methods=['GET', 'PUT'])
+def gets_by_id():
 
-	@api.expect(a_parcel, Validate=True)
-	def put(self):
-
-		"Place entire parcel"
-
-		post = api.payload
-		Parcel.create(post)
-
-		return {"accepted": post}, 202
-
-@api.route(base_url + '/parcel/<int:id>')
-class Parcel_by_id(Resource):
-	def get(self, id):
-
-		"get by id"
+	if request.method == 'GET':
+		if type(id) != Int:
+			abort(400, 'Bad request')
 
 		result = [prod for prod in parcels if prod['id'] == id]
-
 		if result == []:
-			return {"message": "nothing here"}, 204
+			return jsonify({"message": "nothing here"}), 204
 
-		return result, 200
+		return jsonify({result}), 200
 
+	if request.method == 'PUT':
 
-@app.route(base_url + '/parcel/<int:parcel_id>', methods=['PUT'])
-def update_parcel(parcel_id):
+		if not parcel_id or parcel_id < 1:
+			abort(400, 'Bad parcel request')
 
+		update_parcel_by_id = request.get_json()
+		for item in parcels:
+			if update_parcel_by_id['id'] == item['id']:
+				parcels.update(update_parcel_by_id)
 
-	if not parcel_id or parcel_id < 1:
+	return jsonify({"updated": update_parcel_by_id}), 201
+
+@app.route(base_url + '/users/<int:user_id>', methods=['GET'])
+def get_from_user():
+	if not user_id or user_id < 1:
+		abort(400, 'Bad user request')
+		
+	if type(user_id) != Int:
 		abort(400, 'Bad request')
 
-	data = request.get_json()
+		result = [prod for prod in parcels if prod['user_id'] == user_id]
+		if result == []:
+			return jsonify({"message": "nothing here"}), 204
 
-	nickname = data['nickname'],
-	height = data['height'],
-	width= data['width'], 
-	destination = data['destination'],
-	pickup = data['pickup']
+		return jsonify({result}), 200
 
-	new_update = dict(nickname= nickname,
-		height= height,
-		width =width,
-		destination= destination,
-		pickup=pickup)
 
-	for parcel in parcels:
-		if parcel['parcel_id'] == parcels['id']:
-			parcels.append(new_update)
+# @app.route(base_url + '/parcel/<int:parcel_id>', methods=['PUT'])
+# def update_parcel(parcel_id):
 
-			return jsonify({"message": "updated"}), 202
 
-		return jsonify({"message": "parcel not found"}), 200
+# 	if not parcel_id or parcel_id < 1:
+# 		abort(400, 'Bad request')
+
+# 	data = request.get_json()
+
+# 	nickname = data['nickname'],
+# 	height = data['height'],
+# 	width= data['width'], 
+# 	destination = data['destination'],
+# 	pickup = data['pickup']
+
+# 	new_update = dict(nickname= nickname,
+# 		height= height,
+# 		width =width,
+# 		destination= destination,
+# 		pickup=pickup)
+
+# 	for parcel in parcels:
+# 		if parcel['parcel_id'] == parcels['id']:
+# 			parcels.append(new_update)
+
+# 			return jsonify({"message": "updated"}), 202
+
+# 		return jsonify({"message": "parcel not found"}), 200
 
 
 
