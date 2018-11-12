77d623a79456703c5f87d8ab8aa58b54a04f1dd1
Daniel Lwetabe <dannylwe@gmail.com>
2018-11-12 10:04:22 +0300

    updated test. POST confirms.

--- api/test_app.py
+++ api/test_app.py
@@ -2,6 +2,7 @@ from unittest import TestCase
 import json
 from views.app import app
 
+
 class Test(TestCase):
 
 	def setUp(self):
@@ -10,15 +11,22 @@ class Test(TestCase):
 	def test_hello_world(self):
 		response = self.app.get('/api/v1/hello')
 		self.assertEqual(response.status_code, 200)
+
 	def test_parcels_no_post(self):
 		response = self.app.get('/api/v1/parcels')
 		self.assertEqual(response.status_code, 200)
-	# def test_parcels_with_posts(self):
-	# 	response = self.app.post('/api/v1/parcels', data= json.dumps({"height": "tall","pickup": 11,"nickname": "updated","width": 10,"destination": "string"}))
-	# 	self.assertEqual(response.status_code, 200)
+
+	def test_parcels_with_posts(self):
+		response = self.app.post('/api/v1/parcels', 
+			data = 
+			json.dumps({"height": 5, "pickup":"kampala","nickname": "mum's flowers","width": 10,"destination": "gulu"}),
+			content_type="application/json")
+		self.assertEqual(response.status_code, 201)
+
 	def test_parcels_int(self):
 		response = self.app.get('/api/v1/parcels/1')
 		self.assertEqual(response.status_code, 204)
+
 	def test_parcels_by_user(self):
 		response = self.app.get('/users/1/parcels')
 		self.assertEqual(response.status_code, 404)
--- api/views/app.py
+++ api/views/app.py
@@ -19,16 +19,14 @@ def hello_world():
 
 @app.route(base_url + '/parcels', methods=['GET', 'POST'])
 def get_parcel():
+
 	if request.method == 'GET':
 		if parcels == []:
 			return jsonify({"message": "list is empty"}), 200
-
 		return jsonify({"message":parcels}), 200
 
 	if request.method == 'POST':
 		post_parcel = request.get_json()
-		#Parcel.create(post_parcel)
-
 		create(post_parcel)
 
 		return jsonify({"created": post_parcel}), 201
@@ -41,14 +39,15 @@ def gets_by_id(id):
 			abort(400, 'No string literal allowed')
 
 		result = [prod for prod in parcels if prod['id'] == id]
+
 		if result == []:
 			return jsonify({"message": "nothing here"}), 204
-
 		return jsonify({"message": result}), 200
 
 
 @app.route(base_url + '/users/<int:user_id>/parcels', methods=['GET'])
 def get_from_user(user_id):
+
 	if user_id < 1:
 		abort(400, 'Bad user request')
 
@@ -62,10 +61,6 @@ def get_from_user(user_id):
 	for users in parcels:
 		if users['user_id'] == user_id:
 			result.append(users)
-		# else: 
-		# 	if result == []:
-		# 		abort(400, 'No such user')
-
 	return jsonify({"message": result})
 
 
