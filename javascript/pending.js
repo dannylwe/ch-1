634d9da325954ab93659471856b5a5476c254b36
Daniel Lwetabe <acehanks@gmail.com>
2018-12-09 00:56:05 +0300

    working on getting pending orders by user, not complete. Can get data from api, view in console

--- javascript/pending.js
+++ javascript/pending.js
@@ -2,21 +2,37 @@ window.addEventListener("load", e =>{
     //updateParcel();
     axiosUpdate();
 });
-const place = document.getElementById("api");
 
 // async function updateParcel(){
 //     console.log("starting");
-//     const res = fetch("https://challenge3andela.herokuapp.com/api/v1/parcels", {credentials: 'include'})
+//     const res = fetch("https://challenge3andela.herokuapp.com/api/v1/parcels", 
+//     {credentials: 'include'})
 //     .then(rel=>console.log(rel.json()))
 //     .catch(err=>console.log(err));
 //     const result = res.json();
 //     // place.innerText = result;
 // }
+const table = document.getElementById("f-api");
 
 function axiosUpdate(){
     const entry = axios.get("https://challenge3andela.herokuapp.com/api/v1/parcels", 
     {withCredentials: true})
-    .then(res=>console.log(res))
+    .then(res=> {
+        let all= res.data['item info'];
+        console.log(all);
+
+        let table = document.createElement('table');
+        for (one of all){
+            let destination = one.destination;
+            let nickname = one.nickname;
+            let id = one['parcel_id'];
+            let pickup = one.pickup;
+            let status = one.status;
+            let weight = one.weight
+
+            console.log(destination, nickname, id, pickup, status, weight);
+        }
+        document.body.appendChild(table);
+    })
     .catch(err=>console.log(err));
-     place.innerText = entry.then(res=>res.data);
 }
