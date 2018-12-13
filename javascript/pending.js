4e4597543cfe89657f6c720ec4ea7878de785e3e
Daniel Lwetabe <acehanks@gmail.com>
2018-12-13 17:58:04 +0300

    added delete button to pending parcels (all parcels) and depreciated cancel-item tab

--- javascript/pending.js
+++ javascript/pending.js
@@ -29,15 +29,25 @@ function axiosUpdate(){
         let output = '<div>' + 
         '<th>Parcel Id</th><th>Destination</th><th>Pickup</th><th>Nickname</th><th>Status</th>'
         res.data['item info'].forEach(post => {
-            
+            if(post.status==='cancelled'){
+                output += `<tr>
+                <td style = "width: 10%">${post.parcel_id}</td>
+                <td style = "width: 20%">${post.destination}</td>
+                <td style = "width: 22%">${post.pickup}</td>
+                <td style = "width: 15%">${post.nickname}</td>
+                <td style = "width: 10%">${post.status}</td>
+                <td><i class="far fa-times-circle" id="cancel-sign" style="display:none;"></i></td>    
+                </tr>`
+            }else{
             output += `<tr>
             <td style = "width: 10%">${post.parcel_id}</td>
             <td style = "width: 20%">${post.destination}</td>
             <td style = "width: 22%">${post.pickup}</td>
             <td style = "width: 15%">${post.nickname}</td>
-            <td>${post.status}</td>
-            <td><i class="far fa-times-circle"></i></td>
+            <td style = "width: 10%">${post.status}</td>
+            <td><i class="far fa-times-circle" id="cancel-sign" onclick=cancelParcel(${post.parcel_id});></i></td>
             </tr>`
+            }
         });
         output += '</div>';
         divInput.innerHTML = output;
@@ -52,3 +62,13 @@ function successfulHTML(res){
     return '<pre>' + JSON.stringify(res.data['item info'], null, '\t') + '</pre>'; 
     
 };
+
+function cancelParcel(id){
+
+    const entry = "https://challenge3andela.herokuapp.com/api/v1/parcels/"+id+"/cancel";
+    axios.put(entry, id, {withCredentials: true}).then(res=>{
+        console.log("item of id "+ id + " has been deleted");
+        window.location.href = "pending.html"
+    })
+}
+
