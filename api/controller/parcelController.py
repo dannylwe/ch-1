90cdf4194339077dd5ef8fa222f08edab7b50a21
Daniel Lwetabe <dannylwe@gmail.com>
2018-12-30 17:38:19 +0300

    completed challenge 2 refactor completed adding parcel controller

---
+++ api/controller/parcelController.py
@@ -0,0 +1,54 @@
+from flask import Flask, abort, request, jsonify, abort, make_response
+from api.models.parcel_store import *
+
+class Parcel:
+
+	def __init__(self):
+		pass
+
+	def helloWorld():
+		return jsonify({'hello': 'world'}), 200
+
+	def getAllParcels():
+		if parcels == []:
+			return jsonify({"message": "list is empty"}), 200
+		return jsonify({"message":parcels}), 200
+
+	def postSingleParcel():
+		post_parcel = request.get_json()
+
+		error_handler(post_parcel)
+
+		if post_parcel['weight'] > 10:
+			abt_weight = make_response("Too heavy")
+			abt_weight.status_code = 400
+			return abt_weight
+
+		create(post_parcel)
+
+		return jsonify({"created": post_parcel}), 201
+
+	def parcelById(id):
+		result = [prod for prod in parcels if prod['id'] == id]
+
+		if result == []:
+			return jsonify({"message": "nothing here"}), 200
+		return jsonify({"message": result}), 200
+
+	def parcelGetByUserId(user_id):
+		result = []
+
+		for users in parcels:
+			if users['user_id'] == user_id:
+				result.append(users)
+
+		if result == []:
+			abort(404, 'No such user')
+		return jsonify({"message": result})
+
+	def parcelCancelById(id):
+		for parcel in parcels:
+			if parcel['id'] == id:
+				parcel['status'] = "cancelled"
+				return jsonify({"cancelled": parcel}), 201
+			return jsonify({"message": "Id does not exist"}), 200
--- api/views/app.py
+++ api/views/app.py
@@ -1,10 +1,9 @@
-from flask import Flask, abort, request, jsonify, abort, make_response
 from flask_cors import CORS
 from werkzeug.contrib.fixers import ProxyFix
-import datetime
 #import uuid
 from api.models.parcel_store import *
 from api.database.dataBase import Database
+from api.controller.parcelController import Parcel
 
 app = Flask(__name__)
 CORS(app)
@@ -16,80 +15,27 @@ base_url= '/api/v1'
 
 @app.route(base_url + '/hello')
 def hello_world():
-	return jsonify({'hello': 'world'}), 200
+	return Parcel.helloWorld()
 
 @app.route(base_url + '/parcels', methods=['GET', 'POST'])
 def get_parcel():
 
 	if request.method == 'GET':
-		if parcels == []:
-			return jsonify({"message": "list is empty"}), 200
-		return jsonify({"message":parcels}), 200
+		return Parcel.getAllParcels()
 
 	if request.method == 'POST':
-		post_parcel = request.get_json()
-
-		error_handler(post_parcel)
-
-		if post_parcel['weight'] > 10:
-			abt_weight = make_response("Too heavy")
-			abt_weight.status_code = 400
-			return abt_weight
-
-		create(post_parcel)
-
-		return jsonify({"created": post_parcel}), 201
+		return Parcel.postSingleParcel()
 
 @app.route(base_url + '/parcels/<int:id>', methods=['GET'])
 def gets_by_id(id):
 
 	if request.method == 'GET':
-		if type(id) != int:
-			abort(400, 'No string literal allowed')
-
-		result = [prod for prod in parcels if prod['id'] == id]
-
-		if result == []:
-			return jsonify({"message": "nothing here"}), 200
-		return jsonify({"message": result}), 200
-
+		return Parcel.parcelById(id)
 
 @app.route(base_url + '/users/<int:user_id>/parcels', methods=['GET'])
 def get_from_user(user_id):
-
-	result = []
-
-	for users in parcels:
-		if users['user_id'] == user_id:
-			result.append(users)
-
-	if result == []:
-		abort(404, 'No such user')
-	return jsonify({"message": result})
+	return Parcel.parcelGetByUserId(user_id)
 
 @app.route(base_url + '/parcels/<int:id>/cancel', methods=['PUT'])
 def cancel_order(id):
-
-	for parcel in parcels:
-		if parcel['id'] == id:
-			parcel['status'] = "cancelled"
-			return jsonify({"cancelled": parcel}), 201
-	return jsonify({"message": "Id does not exist"}), 200
-
-# not required for challenge 2
-
-# @app.route(base_url + '/register', methods=['POST'])
-# def register_user():
-
-# 	user_info = request.get_json()
-# 	db = Database()
-
-# 	query_sql = """INSERT INTO USERS (email, password, handphone, username) VALUES (%s,
-# 	%s, %s, %s)"""
-
-# 	query_info = (user_info['email'], user_info['password'], user_info['handphone'],
-# 	 user_info['username'])
-
-# 	db.insert(query_sql, query_info)
-
-# 	return jsonify({"Register message": "Succesfully registerd to sendIT"}), 200
+	return Parcel.parcelCancelById(id)
