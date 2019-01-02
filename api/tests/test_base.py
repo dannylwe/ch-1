5d81915d54d76a2cd38d90e6967af9c88afa2ae9
Daniel Lwetabe <dannylwe@gmail.com>
2019-01-03 00:37:16 +0300

    [chore] removed unused/commented code

--- api/tests/test_base.py
+++ api/tests/test_base.py
@@ -17,14 +17,6 @@ class BaseTest(TestCase):
 		print(response.data)
 		self.assertEqual(response.status_code, 400)
 
-	# def test_register_user_success(self):
-	# 	response= self.app.post(base_url + '/auth/user',
-	# 		data=json.dumps({"username":"user2", "email":"user1@gmail.com",
-	# 			"password":"abcdefghij", "handphone":772504771}),content_type="application/json")
-	# 	print(response.data)
-	# 	#201
-	# 	self.assertEqual(response.status_code, 201)
-
 	def test_register_user_exists(self):
 		response= self.app.post(base_url + '/auth/user',
 			data=json.dumps({"username":"user2", "email":"user2@gmail.com",
@@ -90,19 +82,3 @@ class BaseTest(TestCase):
 		return self.app.post(base_url + '/auth/login',
 			data=json.dumps({"username":"user2", "password":"abcdefghij"}),
 			content_type="application/json")
-
-
-
-
-	# def test_register_user_pass(self):
-	# 	response= self.app.post('api/v1/auth/user',
-	# 		data=json.dumps({"username":"danny", "email":"danny@gmail.com",
-	# 			"password":"abcd", "handphone": 772504771}),content_type="application/json")
-	# 	print(response.json)
-	# 	self.assertEqual(response.status_code, 201)
-
-	# def test_login_user(self):
-	# 	response= self.app.post('api/v1/auth/login',
-	# 		data=json.dumps({"username":"user", "email":"user1@gmail.com",
-	# 			"password":"abcd", "handphone":772504771}),content_type="application/json")
-	# 	self.assertEqual(response.status_code, 201)
